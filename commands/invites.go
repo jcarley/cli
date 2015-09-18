cc92b5399c237eab58f376a8395653db5198655a
Josh Ault <josh.ault3@gmail.com>
2015-09-18 10:42:36 -0500

    alert the user when no invites are pending rather than printing an empty list

--- commands/invites.go
+++ commands/invites.go
@@ -23,6 +23,10 @@ func InviteUser(email string, settings *models.Settings) {
 func ListInvites(settings *models.Settings) {
 	helpers.SignIn(settings)
 	invites := helpers.ListEnvironmentInvites(settings)
+	if len(*invites) == 0 {
+		fmt.Printf("There are no pending invites for %s\n", settings.EnvironmentName)
+		return
+	}
 	fmt.Printf("Pending invites for %s:\n", settings.EnvironmentName)
 	for _, invite := range *invites {
 		fmt.Printf("\t%s %s\n", invite.Email, invite.Code)
