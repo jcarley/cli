5e77f1351f28e68f3638003360be06762b4c8c8e
Nathan Sweet <nathanjsweet@gmail.com>
2016-11-16 17:27:04 -0600

    updating gcm removing spinner

--- vendor/github.com/catalyzeio/gcm/gcm/gcm.go
+++ vendor/github.com/catalyzeio/gcm/gcm/gcm.go
@@ -9,20 +9,17 @@ import (
 )
 
 const (
-	chunkSize = 1024 * 1024 // 1 MB
+	chunkSize = 1024 * 1024 // 1 MiB
 
 	// AAD (Additional authenticated data) is to be used in the GCM algorithm
 	AAD = "7f57c07ee9459ed704d5e403086f6503"
 )
 
-// EncryptFile encrypts the file at the specified path using GCM
-func EncryptFile(inFilePath, outFilePath string, key, givenIV, aad []byte) error {
+// EncryptFile encrypts the file at the specified path using GCM.
+func EncryptFile(inFilePath, outFilePath string, key, iv, aad []byte) error {
 	if _, err := os.Stat(inFilePath); os.IsNotExist(err) {
 		return fmt.Errorf("A file does not exist at %s", inFilePath)
 	}
-	// copy the IV since it will potentially be incremented
-	iv := make([]byte, len(givenIV))
-	copy(iv, givenIV)
 
 	inFile, err := os.Open(inFilePath)
 	if err != nil {
@@ -36,85 +33,204 @@ func EncryptFile(inFilePath, outFilePath string, key, givenIV, aad []byte) error
 	}
 	defer outFile.Close()
 
-	aes, err := aes.NewCipher(key)
-	if err != nil {
-		return err
-	}
-	gcm, err := cipher.NewGCMWithNonceSize(aes, len(iv))
+	r, err := NewEncryptReader(inFile, key, iv, aad)
 	if err != nil {
 		return err
 	}
 
-	chunk := make([]byte, chunkSize)
-	written := false
-	for {
-		read, err := inFile.Read(chunk)
-		// ensure we have written at least one chunk before breaking
-		if read > 0 || !written {
-			encrChunk := gcm.Seal(nil, iv, chunk[:read], aad)
-			if _, err := outFile.Write(encrChunk); err != nil {
-				return err
-			}
-			written = true
-		}
-		if err == io.EOF {
-			break
-		}
-		incrementIV(iv)
+	_, err = io.Copy(outFile, r)
+	return err
+}
+
+// DecryptFile decrypts the file at the specified path using GCM.
+func DecryptFile(inFilePath, outFilePath string, key, iv, aad []byte) error {
+	if _, err := os.Stat(inFilePath); os.IsNotExist(err) {
+		return fmt.Errorf("A file does not exist at %s", inFilePath)
 	}
+
+	inFile, err := os.Open(inFilePath)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	outFile, err := os.OpenFile(outFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	w, err := NewDecryptWriteCloser(outFile, key, iv, aad)
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	_, err = io.Copy(w, inFile)
+	return err
+}
+
+// Wraps data from an io.Reader in an encrypted GCM data stream.
+type EncryptReader struct {
+	src io.Reader
+	eof bool
+
+	gcm cipher.AEAD
+	iv  []byte
+	aad []byte
+
+	sealed []byte
+	off    int
+
+	buff []byte
+}
+
+func NewEncryptReader(src io.Reader, key, iv, aad []byte) (*EncryptReader, error) {
+	// copy the IV since it will be incremented
+	ivCopy := make([]byte, len(iv))
+	copy(ivCopy, iv)
+
+	aes, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCMWithNonceSize(aes, len(iv))
+	if err != nil {
+		return nil, err
+	}
+	return &EncryptReader{
+		src: src,
+
+		gcm: gcm,
+		iv:  ivCopy,
+		aad: aad,
+
+		sealed: []byte{},
+
+		buff: make([]byte, chunkSize),
+	}, nil
+}
+
+func (r *EncryptReader) Read(p []byte) (int, error) {
+	n := len(p)
+	off := 0
+	for off < n {
+		// encrypt the next chunk if no data available
+		if r.off >= len(r.sealed) {
+			if r.eof {
+				return off, io.EOF
+			}
+			if err := r.seal(); err != nil {
+				return off, err
+			}
+		}
+		// copy encrypted data from the current chunk
+		read := copy(p[off:], r.sealed[r.off:])
+		r.off += read
+		off += read
+	}
+	return off, nil
+}
+
+func (r *EncryptReader) CalculateTotalSize(size int) int {
+	parts := size / chunkSize
+	if size%chunkSize > 0 {
+		parts++
+	}
+	return r.gcm.Overhead()*parts + size
+}
+
+func (r *EncryptReader) seal() error {
+	// pull in the next chunk from the reader
+	n, err := io.ReadFull(r.src, r.buff)
+	if err == io.EOF || err == io.ErrUnexpectedEOF {
+		// mark EOF reached for subsequent seal attempts
+		r.eof = true
+	} else if err != nil {
+		// some other problem; abort
+		return err
+	}
+	// encrypt this chunk
+	r.sealed = r.gcm.Seal(nil, r.iv, r.buff[:n], r.aad)
+	incrementIV(r.iv)
+	r.off = 0
 	return nil
 }
 
-// DecryptFile decrypts the file at the specified path using GCM
-func DecryptFile(inFilePath, outFilePath string, key, givenIV, aad []byte) error {
-	if _, err := os.Stat(inFilePath); os.IsNotExist(err) {
-		return fmt.Errorf("A file does not exist at %s", inFilePath)
-	}
-	// copy the IV since it will potentially be incremented
-	iv := make([]byte, len(givenIV))
-	copy(iv, givenIV)
+// Unwraps an encrypted GCM data to the given io.Writer stream.
+type DecryptWriteCloser struct {
+	dst io.WriteCloser
 
-	inFile, err := os.Open(inFilePath)
-	if err != nil {
-		return err
-	}
-	defer inFile.Close()
+	gcm cipher.AEAD
+	iv  []byte
+	aad []byte
 
-	outFile, err := os.OpenFile(outFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
-	if err != nil {
-		return err
-	}
-	defer outFile.Close()
+	sealed []byte
+	off    int
+}
+
+func NewDecryptWriteCloser(dst io.WriteCloser, key, iv, aad []byte) (*DecryptWriteCloser, error) {
+	// copy the IV since it will be incremented
+	ivCopy := make([]byte, len(iv))
+	copy(ivCopy, iv)
 
 	aes, err := aes.NewCipher(key)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	gcm, err := cipher.NewGCMWithNonceSize(aes, len(iv))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	written := false
-	chunk := make([]byte, chunkSize+gcm.Overhead())
-	for {
-		read, err := inFile.Read(chunk)
-		// ensure we have written at least one chunk before breaking
-		if read > 0 || !written {
-			decrChunk, err := gcm.Open(nil, iv, chunk[:read], aad)
-			if err != nil {
-				return err
+	return &DecryptWriteCloser{
+		dst: dst,
+
+		gcm: gcm,
+		iv:  ivCopy,
+		aad: aad,
+
+		sealed: make([]byte, chunkSize+gcm.Overhead()),
+	}, nil
+}
+
+func (w *DecryptWriteCloser) Write(p []byte) (int, error) {
+	n := len(p)
+	off := 0
+	for off < n {
+		// copy encrypted data into the current chunk
+		written := copy(w.sealed[w.off:], p[off:])
+		w.off += written
+		off += written
+		// decrypt if chunk is finished
+		if w.off == cap(w.sealed) {
+			if err := w.open(); err != nil {
+				return off, err
 			}
-			if _, err := outFile.Write(decrChunk); err != nil {
-				return err
-			}
-			written = true
 		}
-		if err == io.EOF {
-			break
-		}
-		incrementIV(iv)
 	}
+	return off, nil
+}
+
+func (w *DecryptWriteCloser) Close() error {
+	if w.off > 0 {
+		if err := w.open(); err != nil {
+			return err
+		}
+	}
+	return w.dst.Close()
+}
+
+func (w *DecryptWriteCloser) open() error {
+	opened, err := w.gcm.Open(nil, w.iv, w.sealed[:w.off], w.aad)
+	if err != nil {
+		return err
+	}
+	if _, err := w.dst.Write(opened); err != nil {
+		return err
+	}
+	incrementIV(w.iv)
+	w.off = 0
 	return nil
 }
 
