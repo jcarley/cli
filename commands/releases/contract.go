4cf9bb01f4b3adc0ae2ab4099e4f0dc71fde3ffa
abeachcatalyze <abeach@catalyze.io>
2017-02-21 11:32:37 -0600

    Releases Documentation Change COPS-863 Add a statement showing how many past releases will be kept We should specify that we will only be keeping the last three releases. We prune the previous releases to save on disk space on docker registry

--- commands/releases/contract.go
+++ commands/releases/contract.go
@@ -18,6 +18,8 @@ var Cmd = models.Command{
 		"A release is automatically created each time you perform a git push. " +
 		"The release is tagged with the git SHA of the commit. " +
 		"Releases are a way of tagging specific points in time of your git history. " +
+		"By default, the last three releases will be kept. " +
+		"Please contact Support if you require more than the last three releases to be retained. " +
 		"You can rollback to a specific release by using the [rollback](#rollback) command. " +
 		"The releases command cannot be run directly but has sub commands.",
 	CmdFunc: func(settings *models.Settings) func(cmd *cli.Cmd) {
