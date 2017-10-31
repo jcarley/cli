63028391092e11c2d0241de7f2c687537fb5ab60
forana <alex@alexforan.com>
2017-10-31 12:13:16 -0500

    bump version to 4.1.0 and fixed test

--- config/constants.go
+++ config/constants.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	// VERSION is the current cli version
-	VERSION = "4.0.1"
+	VERSION = "4.1.0"
 	// Beta determines whether or not this is a beta build of the CLI
 	Beta = false
 	// AccountsHost is the production accounts URL
